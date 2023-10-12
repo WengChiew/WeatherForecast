285dd6cfae6cffa82ed2895c9d9666693c2a5739
Weng Chiew <wengchiew@outlook.com>
2023-10-12 14:12:43 +0200

    maisu

--- src/main/java/com/weng/weatherforecast/controller/WeatherController.java
+++ src/main/java/com/weng/weatherforecast/controller/WeatherController.java
@@ -1,11 +1,13 @@
 package com.weng.weatherforecast.controller;
 
 import com.weng.weatherforecast.DTO.WeatherForecastDTO;
-import com.weng.weatherforecast.exceptions.LatitudeOutOfBoundException;
-import com.weng.weatherforecast.exceptions.LocationNotSpecifiedException;
-import com.weng.weatherforecast.exceptions.LongitudeOutOfBoundsException;
+import com.weng.weatherforecast.Service.WeatherForecastService;
+import com.weng.weatherforecast.exceptions.*;
+import com.weng.weatherforecast.model.Coordinates;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +16,19 @@ import java.util.List;
 @RestController
 public class WeatherController {
 
+    @Autowired
+    private WeatherForecastService weatherForecastService;
+
     @GetMapping("/forecast")
-    public ResponseEntity<List<WeatherForecastDTO>>weatherForecastDTOList(@RequestParam(required = false) String location, @RequestParam(required = false)  Double latitude, @RequestParam(required = false) Double longitude, @RequestParam int days, @RequestParam String apikey){
+    public ResponseEntity<List<WeatherForecastDTO>>weatherForecastDTOList(@RequestParam(required = false) String location, @RequestParam(required = false)  Double latitude, @RequestParam(required = false) Double longitude, @RequestParam int days, @RequestHeader(value = "apikey", required = false) String apikey){
+
+        if(apikey ==null ){
+            throw new ApiKeyNotFoundException("You are not authorized to access this app");
+        }
+        if(!apikey.equals("mY+0p$EcrE+")){
+            throw new WrongApiKeyException("Wrong Api Key loser");
+        }
+
         if(location==null){
             if(longitude==null || latitude==null) {
                 throw new LocationNotSpecifiedException("No location data was given");
@@ -41,7 +54,11 @@ public class WeatherController {
         } else if (longitude != null || latitude != null){
             throw new LocationNotSpecifiedException("Only give one Location data you poopoo");
         } else {
+            Coordinates coordinates = weatherForecastService.getCoordinates(location);
+        }
 
+        if(days > 14 || days < 1){
+            throw new DaysOutOfBoundException("Days should be between 1 and 14");
         }
 
 
